189e03e76ea92655317f6b82295af80746fc5414
Hexufaa <130238334+Hexufaa@users.noreply.github.com>
2024-07-15 20:03:12 -0300

    implementado pulo e diferente tipo de movimentação

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,23 +6,46 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
 
+    public float forceJump = 2;
+
     private void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
             myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
         }
+
+        if(myRigidBody.velocity.x > 0)
+        {
+            myRigidBody.velocity += friction;
+        }else if(myRigidBody.velocity.y < 0)
+        {
+            myRigidBody.velocity -= friction;
+        }
     }
 
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            myRigidBody.velocity = Vector2.up * forceJump;
+        }
+    }
 
 }
