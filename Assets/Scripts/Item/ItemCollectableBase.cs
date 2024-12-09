7748b2b8d9de1ae3baf3ed08d15210805c633d48
Hexufaa <130238334+Hexufaa@users.noreply.github.com>
2024-12-09 19:46:31 -0300

    erro no emissor de particulas

--- Assets/Scripts/Item/ItemCollectableBase.cs
+++ Assets/Scripts/Item/ItemCollectableBase.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemCollectableBase : MonoBehaviour
 {
-
+    
     public string compareTag = "Player";
-
-
+    public float timeToHide = 3f;
+    public ParticleSystem particleSystem;
+    public GameObject graphicItem;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -19,14 +21,21 @@ public class ItemCollectableBase : MonoBehaviour
 
     protected virtual void Collect()
     {
-        gameObject.SetActive(false);
         OnCollect();
-        
     }
 
     protected virtual void OnCollect()
     {
-
+        if (graphicItem != null)
+        { graphicItem.SetActive(false); }
+        Invoke(nameof(HideObject), timeToHide);
+        if (particleSystem != null) particleSystem.Play();
     }
+
+    private void HideObject()
+    {
+        gameObject.SetActive(false);
+    }
+    
 
 }
